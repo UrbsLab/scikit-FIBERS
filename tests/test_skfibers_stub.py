19320183535bd921d948a250e5a47844fe613019
Harsh Bandhey <raptor419heavy@gmail.com>
2023-03-02 19:29:24 -0800

    Stub Test file

--- tests/test_skfibers_stub.py
+++ tests/test_skfibers_stub.py
@@ -1,12 +1,9 @@
-import timeit
 import unittest
-from typing import Dict, Any
-import pandas as pd
 from skfibers.fibers import FIBERS
 
 
 def test_rare_stub():
-    pass
+    obj = FIBERS()
 
 
 if __name__ == '__main__':
