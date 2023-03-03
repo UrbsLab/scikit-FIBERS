53b8a2a8e7d307f024c70448c38ef326dd869c40
Harsh Bandhey <raptor419heavy@gmail.com>
2023-03-02 19:28:30 -0800

    Removed incorrect RARE tests

---
+++ tests/test_skfibers_stub.py
@@ -0,0 +1,13 @@
+import timeit
+import unittest
+from typing import Dict, Any
+import pandas as pd
+from skfibers.fibers import FIBERS
+
+
+def test_rare_stub():
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
