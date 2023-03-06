05969fe8017b6199c8dd3c1922bbec35657c46a4
Harsh Bandhey <raptor419heavy@gmail.com>
2023-03-06 15:19:59 -0800

    Removed RARE

--- src/skfibers/__init__.py
+++ src/skfibers/__init__.py
@@ -4,6 +4,8 @@ scikit-fibers.
 A Scikit Learn compatible implementation of FIBERS Algorithm.
 """
 
+from .fibers import FIBERS
+
 __version__ = "0.9.2"
 __author__ = 'Harsh Bandhey'
 __credits__ = 'Cedars-Sinai Health Systems'
