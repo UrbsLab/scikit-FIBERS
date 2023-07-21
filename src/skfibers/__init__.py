61b48d628362ef4c38a7ab641f2f832281ff7d32
Sphia Sadek <isadeks@gmail.com>
2023-07-21 14:27:47 -0400

    pushing new code

--- src/skfibers/__init__.py
+++ src/skfibers/__init__.py
@@ -4,7 +4,7 @@ scikit-fibers.
 A Scikit Learn compatible implementation of FIBERS Algorithm.
 """
 
-from .fibers import FIBERS
+from .new_fibers import FIBERS
 
 __version__ = "0.9.3"
 __author__ = 'Harsh Bandhey'
