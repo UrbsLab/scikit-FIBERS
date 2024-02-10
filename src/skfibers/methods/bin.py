b6b57a273be968f045fcf8822f644ab0207af9f3
ryanu <ryanurb@upenn.edu>
2024-02-10 10:44:43 -0800

    Working update

--- src/skfibers/methods/bin.py
+++ src/skfibers/methods/bin.py
@@ -1,10 +1,10 @@
-import numpy as np
+#import numpy as np
 import pandas as pd
 import random
 import copy
-from lifelines import CoxPHFitter
+#from lifelines import CoxPHFitter
 from lifelines.statistics import logrank_test
-from scipy.stats import ranksums
+#from scipy.stats import ranksums
 
 class BIN:
     def __init__(self):
@@ -17,6 +17,7 @@ class BIN:
         self.low_risk_count = None
         self.high_risk_count = None
         self.birth_iteration = None
+
 
     def initialize_random(self,feature_names,min_bin_size,max_bin_init_size,group_thresh,min_thresh,max_thresh,iteration,random_seed):
         self.birth_iteration = iteration
@@ -31,18 +32,14 @@ class BIN:
             self.group_threshold = random.randint(min_thresh,max_thresh)
     
 
-
     def evaluate(self,feature_df,outcome_df,censor_df,outcome_type,fitness_metric,outcome_label,
                  censor_label,min_thresh,max_thresh,int_thresh,group_thresh,threshold_evolving,iterations,iteration):
         # Sum instance values across features specified in the bin
         feature_sums = feature_df[self.feature_list].sum(axis=1)
         bin_df = pd.DataFrame({'feature_sum':feature_sums})
-        #print("Bin Data Shape: "+str(bin_df.shape))
 
         # Create evaluation dataframe including bin sum feature with 
-        bin_df = pd.concat([bin_df,outcome_df,censor_df],axis=1) #, ignore_index=True
-        #print("Bin Data Shape2: "+str(bin_df.shape))
-        #print(bin_df.head())
+        bin_df = pd.concat([bin_df,outcome_df,censor_df],axis=1)
 
         if (group_thresh == None and not threshold_evolving) or (group_thresh == None and iteration == iterations-1): #Adaptive thresholding activated (always applied on last iteration)
             # Select best threshold by evaluating all considered
@@ -97,7 +94,7 @@ class BIN:
 
     def uniform_crossover(self,other_offspring,crossover_prob,threshold_evolving,random_seed):
         #create list of feature names unique to one list or another
-        random.seed(random_seed)  
+        #random.seed(random_seed)  
 
         # Convert lists to sets for efficient set operations
         set1 = set(self.feature_list)
@@ -129,7 +126,7 @@ class BIN:
 
     def mutation(self,mutation_prob,feature_names,min_bin_size,max_bin_init_size,threshold_evolving,min_thresh,max_thresh,random_seed):
 
-        random.seed(random_seed) 
+        #random.seed(random_seed) 
 
         if len(self.feature_list) == 0: #Initialize new bin if empty after crossover
             feature_count = random.randint(min_bin_size,max_bin_init_size)
@@ -160,13 +157,16 @@ class BIN:
                     else: # Deletion
                         self.feature_list.remove(feature)
 
-        # Apply crossover to thresholding if threshold_evolving
+        # Apply mutation to thresholding if threshold_evolving
         if threshold_evolving:
             if random.random() < mutation_prob:
-                thresh_count = random.randint(min_thresh,max_thresh)
-                other_thresh = [value for value in thresh_count if value != self.group_threshold] #pick a feature not already in the bin
-                random_thresh = random.choice(other_thresh)
-                self.group_threshold = random_thresh
+                if min_thresh == max_thresh:
+                    pass
+                else:
+                    thresh_list = [i for i in range(min_thresh,max_thresh+1)] #random.randint(min_thresh,max_thresh)
+                    thresh_list.pop(thresh_list.index(self.group_threshold)) #[value for value in thresh_count if value != self.group_threshold] #pick a feature not already in the bin
+                    random_thresh = random.choice(thresh_list)
+                    self.group_threshold = random_thresh
 
 
     def calculate_fitness(self,pareto_fitness,group_strata_min,penalty):
@@ -186,7 +186,7 @@ class BIN:
     def random_bin(self,feature_names,min_bin_size,max_bin_init_size,random_seed):
         """Takes an previously generated offspring bin (that already existed in the pop) and generates an new feature_list """
         # Initialize features in bin
-        random.seed(random_seed)  
+        #random.seed(random_seed)  
         feature_count = random.randint(min_bin_size,max_bin_init_size)
         self.feature_list = random.sample(feature_names,feature_count)
         self.bin_size = len(self.feature_list)
@@ -201,101 +201,8 @@ class BIN:
         return equivalent
     
 
-
-
-    def __iter__(self):
-        self.index = 0
-        return self
-
-    def __next__(self):
-        if self.index < len(self.feature_list):
-            x = self.feature_list[self.index]
-            self.index += 1
-            return x
-        else:
-            raise StopIteration
-
-    def __eq__(self, other):
-        if not isinstance(other, BIN):
-            # don't attempt to compare against unrelated types
-            return NotImplemented
-        return sorted(self.feature_list) == sorted(other.feature_list)
-
-    def __len__(self):
-        return len(self.feature_list)
-
-    def __getitem__(self, item):
-        return self.feature_list[item]
-
-    def __str__(self):
-        return str(self.feature_list)
-
-    def __repr__(self):
-        return str(self)
-
-    def __lt__(self, other):
-        return self.score < other.score
-
-    def __gt__(self, other):
-        return other.__lt__(self)
-
-    # Setter methods of the BIN attributes
-
-    def set_name(self, bin_name):
-        self.bin_name = bin_name
-
-    def set_score(self, score):
-        self.score = score
-
-    def set_threshold(self, threshold):
-        self.threshold = threshold
-
-    def set_feature_list(self, feature_list):
-        self.feature_list = feature_list
-
-    def set_seen(self):
-        self.seen = True
-
-    def set_not_seen(self):
-        self.seen = False
-
-        # Getter methods of the BIN attributes
-
-    def get_feature_list(self):
-        return self.feature_list.copy()
-
-    def get_threshold(self):
-        return self.threshold
-
-    def get_score(self):
-        return self.score
-
-    def get_name(self):
-        return self.bin_name
-
-    def was_seen(self):
-        return self.seen
-
-    # Log rank test used to score the different thresholds and fitness of the BIN
-    def log_rank_test(self, bin_feature_matrix, label_name, duration_name,
-                      informative_cutoff, threshold):
-        score = 0
-        df_0 = bin_feature_matrix.loc[bin_feature_matrix[self.bin_name] <= threshold]
-        df_1 = bin_feature_matrix.loc[bin_feature_matrix[self.bin_name] > threshold]
-
-        durations_no = df_0[duration_name].to_list()
-        event_observed_no = df_0[label_name].to_list()
-        durations_mm = df_1[duration_name].to_list()
-        event_observed_mm = df_1[label_name].to_list()
-
-        if len(event_observed_no) > informative_cutoff * (len(event_observed_no) + len(event_observed_mm)) and len(
-                event_observed_mm) > informative_cutoff * (len(event_observed_no) + len(event_observed_mm)):
-            results = logrank_test(durations_no, durations_mm, event_observed_A=event_observed_no,
-                                   event_observed_B=event_observed_mm)
-            score = results.test_statistic
-
-        return score
-
+    # non functional
+    """
     def residuals_score(self, residuals, bin_feature_matrix, label_name, duration_name,
                         informative_cutoff, threshold):
         score = 0
@@ -340,30 +247,4 @@ class BIN:
         else:
             score = - np.inf
         return score
-
-    # This method will update the threshold for the bin by try all the thresholds from min to max threshold
-    # and uses the threshold that will get the highest score
-    def try_all_thresholds(self, min_threshold, max_threshold, bin_feature_matrix,
-                           label_name, duration_name, informative_cutoff, scoring_method="log_rank",
-                           residuals=None, covariate_matrix=None):
-        # to avoid unnecessary computations, if they have already tried all the thresholds which is determined by the
-        # seen variable, then no need to try all the thresholds
-        if not self.seen:
-            highest_score = 0
-            for threshold in range(min_threshold, max_threshold + 1):
-                # Variable that will store the highest score used to determine the threshold
-                if scoring_method == "log_rank":
-                    score = self.log_rank_test(bin_feature_matrix, label_name,
-                                               duration_name, informative_cutoff, threshold)
-                elif scoring_method == "AIC":
-                    score = self.aic_score(covariate_matrix, bin_feature_matrix, label_name,
-                                           duration_name, informative_cutoff, threshold)
-                elif scoring_method == "residuals":
-                    score = self.residuals_score(residuals, bin_feature_matrix, label_name,
-                                                 duration_name, informative_cutoff, threshold)
-                if score > highest_score:
-                    self.score = score
-                    self.threshold = threshold
-                    highest_score = score
-
-            self.seen = True
+    """
