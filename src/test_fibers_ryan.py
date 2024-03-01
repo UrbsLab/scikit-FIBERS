c070241841f0b7b03942bc79d7887fbde75fa603
ryanu <ryanurb@upenn.edu>
2024-03-01 02:42:48 -0800

    Major Updates to FIBERS and Simulator
    Added diversity pressure, created new data simulator, and started adding simulated covariates to data simulator and a rare SNP dataset simulator. These last to items are not yet complete.

--- src/test_fibers_ryan.py
+++ src/test_fibers_ryan.py
@@ -1,48 +1,38 @@
-#import logging
-#import pytest
-import pandas as pd
 import os
+from skfibers.fibers import FIBERS
+from skfibers.experiments.survival_sim_simple import survival_data_simulation
+from sklearn.metrics import classification_report
+
 current_working_directory = os.getcwd()
 print(current_working_directory)
 
-#from sklearn.pipeline import Pipeline
-from skfibers.fibers import FIBERS
-
-from skfibers.experiments.datagen import create_data_simulation_bin
-from skfibers.experiments.datagen_evolvable_threshold import create_data_simulation_bin_evolve
-
-
-data = create_data_simulation_bin_evolve(number_of_instances=10000, number_of_features=100, number_of_features_in_bin=10,
-                                  no_fail_proportion=0.5, mm_frequency_range=(0.4, 0.5), noise_frequency=0.0,
-                                  class0_time_to_event_range=(1.5, 0.2), class1_time_to_event_range=(1, 0.2),
-                                  censoring_frequency=0.5, random_seed=42, negative=False, threshold=1)
+data = survival_data_simulation(instances=10000, total_features=100, predictive_features=10, low_risk_proportion=0.5, 
+                                threshold = 1, feature_frequency_range=(0.1, 0.4), noise_frequency=0.0, 
+                                class0_time_to_event_range=(1.5, 0.2), class1_time_to_event_range=(1, 0.2), 
+                                censoring_frequency=0.2, covariates_to_sim=0, covariates_signal_range=(0.2,0.4), random_seed=42)
 
 data.to_csv('sampledata.csv', index=False)
-data = pd.read_csv('sampledata.csv')
 true_risk_group = data[['TrueRiskGroup']]
 data = data.drop('TrueRiskGroup', axis=1)
 
-fibers = FIBERS(outcome_label="Duration", outcome_type="survival",iterations=50,
-                    pop_size = 50, crossover_prob=0.5, mutation_prob=0.1, new_gen=1.0, elitism=0.1, min_bin_size=1,
-                    fitness_metric="log_rank", log_rank_weighting=None,censor_label="Censoring", group_strata_min=0.2,
-                    group_thresh=None, min_thresh=0, max_thresh=5, int_thresh=True, thresh_evolve_prob=0.5,
-                    manual_bin_init=None, covariates=None, report=[0,10], random_seed=None, verbose=True)
+fibers = FIBERS(outcome_label="Duration", outcome_type="survival", iterations=50, pop_size=50, tournament_prop=0.5, 
+                crossover_prob=0.5, min_mutation_prob=0.1, max_mutation_prob=0.5, merge_prob=0.1, new_gen=1.0, 
+                elitism=0.1, diversity_pressure=3, min_bin_size=1, max_bin_size=None, max_bin_init_size=10, 
+                fitness_metric="log_rank", log_rank_weighting=None, censor_label="Censoring", 
+                group_strata_min=0.2, penalty=0.5, group_thresh=None, min_thresh=0, max_thresh=3, int_thresh=True, 
+                thresh_evolve_prob=0.5, manual_bin_init=None, covariates=None, report=[0,10,20,30,40], 
+                random_seed=42,verbose=False)
 
 fibers = fibers.fit(data)
 
-print(fibers.top_perform_df)
+fibers.get_bin_report(0)
 
 tdf = fibers.transform(data)
 print(tdf)
 
 predictions = fibers.predict(data,bin_number=0)
-print(predictions)
+print(classification_report(predictions, true_risk_group, digits=8))
 
 predictions = fibers.predict(data)
-print(predictions)
+print(classification_report(predictions, true_risk_group, digits=8))
 
-low_outcome, high_outcome, low_censor, high_censor, bin_report_df = fibers.get_bin_groups(data,0)
-
-feature_names, feature_tracking = fibers.get_feature_tracking()
-print(feature_names)
-print(feature_tracking)
