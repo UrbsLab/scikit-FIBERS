10a84ba39aae8f35777f34cd285110849cf4f1e1
Harsh Bandhey <raptor419heavy@gmail.com>
2024-04-18 19:41:34 -0700

    edit for pypi

--- src/skfibers/__init__.py
+++ src/skfibers/__init__.py
@@ -6,6 +6,6 @@ A Scikit Learn compatible implementation of FIBERS Algorithm.
 
 from .fibers import FIBERS
 
-__version__ = "0.9.3"
+__version__ = "1.0.1"
 __author__ = 'Harsh Bandhey'
 __credits__ = 'Cedars-Sinai Health Systems'
