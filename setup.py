d4984676a7cdf286d4e7a5500d309994496cdb0f
Harsh Bandhey <raptor419heavy@gmail.com>
2024-04-18 19:41:34 -0700

    edit for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scikit-fibers",
-    version="1.0.0",
+    version="1.0.1",
     author="Harsh Bandhey",
     author_email="harsh.bandhey@cshs.org",
     description="A Scikit Learn compatible implementation of FIBERS Algorithm",
