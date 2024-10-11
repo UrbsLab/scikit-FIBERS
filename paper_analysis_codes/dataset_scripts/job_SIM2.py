75fae1791fe8f8907f0d84f21f7b79159b8327f2
ryanu <ryanurb@upenn.edu>
2024-10-11 00:43:47 -0700

    Update job_SIM2.py

--- paper_analysis_codes/dataset_scripts/job_SIM2.py
+++ paper_analysis_codes/dataset_scripts/job_SIM2.py
@@ -17,20 +17,23 @@ def main(argv):
     parser.add_argument('--l', dest='exp_name', help='experiment name dataset label', type=str, default='Sim')
     parser.add_argument('--c', dest='censor', help='censoring frequency in dataset', type=float, default=0.2)
     parser.add_argument('--n', dest='noise', help='proportion of noise in the dataset', type=float, default=0.0)
-
+    parser.add_argument('--nc', dest='nc', help='whether to make this datasets a negative control', type=str, default = 'False') #full path/filename
     options=parser.parse_args(argv[1:])
 
     data_path = options.data_path
     instance = options.instance
     pred_feature = options.pred_feature
+    if options.nc == 'True':
+        nc = True
+    else:
+        nc = False
     nc = False
-    noise = 0
+    noise = options.noise
     total_feature = options.total_feature
-    threshold = 0
-    censor = 0
+    censor = options.censor
     exp_name = options.exp_name
 
-    data_name = exp_name+'_i_'+str(instance)+'_tf_'+str(total_feature)+'_p_'+str(pred_feature)+'_t_'+str(threshold)+'_n_'+str(noise)+'_c_'+str(censor)+'_nc_'+str(nc)
+    data_name = exp_name+'_i_'+str(instance)+'_tf_'+str(total_feature)+'_p_'+str(pred_feature)+'_n_'+str(noise)+'_c_'+str(censor)+'_nc_'+str(nc)
 
     #Generate Example Simulated Dataset --------------------------------------------
     full_data_name_path = data_path +'/'+data_name+'.csv'
@@ -40,7 +43,7 @@ def main(argv):
     #         noise_frequency=noise, class0_time_to_event_range=(1.5, 0.2), class1_time_to_event_range=(1, 0.2), censoring_frequency=censor, 
     #         negative_control=nc, random_seed=42)
     _, data = survival_data_simulation_covariates(instances=instance,total_features=total_feature,
-                                                           predictive_features=pred_feature,feature_frequency_range=(0.1, 0.5),random_seed=None)
+                                                           predictive_features=pred_feature,feature_frequency_range=(0.1, 0.4),noise_frequency=noise,censoring_frequency=censor,negative_control=nc,random_seed=42)
     data.to_csv(full_data_name_path, index=False)
     print('Dataset Simulation Complete')
 
