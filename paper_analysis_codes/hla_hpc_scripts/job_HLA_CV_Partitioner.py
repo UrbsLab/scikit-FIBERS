b4f0f34bf70bbacf0c12430a3bd9a7a566aff3e5
ryanu <ryanurb@upenn.edu>
2024-11-13 14:32:52 -0800

    Update job_HLA_CV_Partitioner.py

--- paper_analysis_codes/hla_hpc_scripts/job_HLA_CV_Partitioner.py
+++ paper_analysis_codes/hla_hpc_scripts/job_HLA_CV_Partitioner.py
@@ -8,7 +8,7 @@ def main(argv):
     #ARGUMENTS:------------------------------------------------------------------------------------
     parser = argparse.ArgumentParser(description='')
     #Script Parameters
-    parser.add_argument('--d', dest='datapath', help='name of data file (REQUIRED)', type=str, default = 'myData') #output folder name
+    parser.add_argument('--d', dest='datafile', help='name of data file (REQUIRED)', type=str, default = 'myData') #output folder name
     parser.add_argument('--o', dest='outputpath', help='directory path to write output (default=CWD)', type=str, default = 'myOutput') #full path/filename
     options=parser.parse_args(argv[1:])
 
