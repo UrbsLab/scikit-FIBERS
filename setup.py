6840b2bf578c9c3b1e11f03a8d544af79e7546a1
Ryan Urbanowicz <ryan.urbanowicz@cshs.org>
2026-08-31 16:33:16 -0700

    version number update for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scikit-fibers",
-    version="2.0.0",
+    version="2.1.0",
     author="Harsh Bandhey, Ryan Urbanowicz",
     author_email="harsh.bandhey@cshs.org",
     description="A Scikit Learn compatible implementation of FIBERS Algorithm",
--- src/skfibers/__init__.py
+++ src/skfibers/__init__.py
@@ -6,6 +6,6 @@ A Scikit Learn compatible implementation of FIBERS Algorithm.
 
 from .fibers import FIBERS
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __author__ = 'Harsh Bandhey, Ryan Urbanowicz'
 __credits__ = 'Cedars-Sinai Health Systems'
